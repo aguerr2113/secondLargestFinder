1b3d63bb880743d759d96b9e3fe774b4c134f71f
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-04-01 14:55:16 -0500

    define an array of integers,initialize variable to capture smallest value,initialize variable to track the second largest

--- App.java
+++ App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // define an array of integers
+        Integer [] mainArray = {10,5,20,8,25,18};
+        // initialize variable to capture smallest value
+        int largest = Integer.MIN_VALUE;
+        // initialize variable to track the second larges
+        int secondLargest = Integer.MIN_VALUE;
+        
         System.out.println("Hello, World!");
     }
 }
